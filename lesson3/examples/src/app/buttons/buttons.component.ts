533375427a60ce210c03d797d1ef99b51ead1110
DonaldHartley <Donald.H.Hartley@gmail.com>
2019-10-28 13:22:08 -0500

    exercise completed

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'buttons',
-   templateUrl: './buttons.component.html',
-   styleUrls: ['./buttons.component.css']
+  selector: "buttons",
+  templateUrl: "./buttons.component.html",
+  styleUrls: ["./buttons.component.css"]
 })
 export class ButtonsComponent implements OnInit {
-   buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+  buttonHeading: string = "Buttons";
+  inactiveG: boolean = false;
+  inactiveS: boolean = false;
+  inactiveC: boolean = false;
+  jokeHeading: string = "Jokes Below!"
+  active: boolean = false;
+  location: string = 'center';
 
-   constructor() { }
+  reactivate(){
+    this.inactiveG= false;
+    this.inactiveS= false;
+    this.inactiveC= false;
+  }
 
-   ngOnInit() { }
+  shiftLocation(oldLocation: string) {
+    while (this.location === oldLocation) {
+       this.location = ['left', 'right', 'center'][Math.floor(Math.random()*3)];
+    }
+    return this.location;
+ }
 
+
+  constructor() {}
+
+  ngOnInit() {}
 }
